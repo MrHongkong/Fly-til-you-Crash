2c21f54fa49b2e31f035a95ebdd39f9fa34e705a
Unknown <jkm.svensson@gmail.com>
2019-01-16 15:35:18 +0100

    Controll

--- Fly til you crash/Assets/Scripts/Player/Movement.cs
+++ Fly til you crash/Assets/Scripts/Player/Movement.cs
@@ -4,28 +4,26 @@ using UnityEngine;
 // Made by Jocke
 public class Movement : MonoBehaviour
 {
-    //public InputMaster controls;
-    public Rigidbody rb;
+    Rigidbody rb;
+    Vector3 movement;
+    public float speed;
 
     private void Awake()
     {
-        controls.Player.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        rb = GetComponent<Rigidbody>();    
     }
 
-    void Move(Vector2 direction)
+    private void Update()
     {
-        Vector2 tmp = new Vector2(transform.position.x, transform.position.y);
-        Vector2 move = tmp + direction;
-        rb.MovePosition(move);
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v, speed);
     }
 
-    private void OnEnable()
+    void Move(float h, float v, float s)
     {
-        controls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        controls.Disable();
+        movement.Set(h, v, 0f);
+        movement = movement.normalized * s * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
     }
 }
