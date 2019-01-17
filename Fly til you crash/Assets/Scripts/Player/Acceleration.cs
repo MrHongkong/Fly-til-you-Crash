b66ee680d7fa685af5e5f18d5d5b673b34d84513
Philip <philipa@hotmail.se>
2019-01-17 12:02:34 +0100

    Acceleration

---
+++ Fly til you crash/Assets/Scripts/Player/Acceleration.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Acceleration : MonoBehaviour
+{
+
+    public float maxSpeed = 1000f;
+    public float timeZeroToMax = 50f;
+    float accelRatePerSec;
+    float forwardVelocity;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        accelRatePerSec = maxSpeed / timeZeroToMax;
+        forwardVelocity = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardVelocity += accelRatePerSec * Time.deltaTime;
+        forwardVelocity = Mathf.Min (forwardVelocity, maxSpeed);
+    }
+
+    void LateUpdate()
+    {
+        rb.velocity = transform.forward * forwardVelocity;   
+    }
+}
