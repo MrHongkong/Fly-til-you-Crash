a9987e838172538708f9376c26aa10aa2565810b
BattleCisco <adam.torngren@hotmail.com>
2019-01-17 14:13:03 +0100

    Reversed bank controls and added player prefab

--- Fly til you crash/Assets/PlayerController.cs
+++ Fly til you crash/Assets/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     public float yAxis;
@@ -17,18 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Add force from jet engine , set enginePower to 15000
-        rb.AddForce(transform.forward * enginePower);
-        
         //Banking controls, turning turning left and right on Z axis
-        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward);
+        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -0.5f);
 
         //Pitch controls, turning the nose up and down
         rb.AddTorque(Input.GetAxis("Vertical") * transform.right * yAxis);
-        
-        //Set drag and angular drag according relative to speed
-        //rb.drag = 0.001f * rb.velocity.magnitude;
-        //rb.angularDrag = 0.01f * rb.velocity.magnitude;
 
         rb.velocity *= 0.9f;
     }
