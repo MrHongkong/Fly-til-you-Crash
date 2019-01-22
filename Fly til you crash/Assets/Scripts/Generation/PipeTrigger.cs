26056ef4b3e15ad1ffd1a3ee67030e3bb0f158f7
Menoka <sebastian_kba@hotmail.com>
2019-01-22 10:41:36 +0100

    Pipe Generation functions on trigger enter

---
+++ Fly til you crash/Assets/Scripts/Generation/PipeTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Created by Sebastian Nilsson
+/// </summary>
+
+public class PipeTrigger : MonoBehaviour
+{
+    private PipeGenerationSebastian generation;
+    private void Start()
+    {
+        generation = GameObject.Find("PipeGenerator").GetComponent<PipeGenerationSebastian>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        generation.GeneratePipe(transform);
+    }
+}
