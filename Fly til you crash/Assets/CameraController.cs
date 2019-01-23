1b78d25ceb8534215ad3cfdb76ad3ec6a05d26e7
Adam <adam.torngren@hotmail.com>
2019-01-23 15:10:33 +0100

    Camera, player, particles and speed changes

---
+++ Fly til you crash/Assets/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public float fov;
+
+    [Range(0.01f, 0.99f)]
+    public float cameraBias;
+
+    [Range(0.01f, 0.99f)]
+    public float cameraAttentionBias;
+    Vector3 attention;
+
+    public void SetFOV(float fov)
+    {
+        Camera.main.fieldOfView = fov;
+        this.fov = fov;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attention = player.position + 25f * player.forward;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 CameraLocation = player.position + -10f * player.forward + 5f * Vector3.up;
+        Camera.main.transform.position = (Camera.main.transform.position * cameraBias) + (CameraLocation * (1f - cameraBias));
+
+        attention = (attention * cameraAttentionBias) + ((player.position + 25f * player.forward) * (1f - cameraAttentionBias));
+        Camera.main.transform.LookAt(attention);
+    }
+}
---
+++ Fly til you crash/Assets/SpeedController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour
+{
+    public float MaxSpeed;
+    public float AccelerationSpeed;
+
+    float e = 2.7182818284590452353602874713527f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float float_velocity = MaxSpeed - MaxSpeed / Mathf.Pow((e), AccelerationSpeed * Time.time);
+        Camera.main.fieldOfView = Mathf.Clamp(float_velocity, 80f, 120f);
+        rb.velocity = float_velocity * transform.forward;
+
+    }
+}
