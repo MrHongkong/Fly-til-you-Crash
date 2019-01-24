cb102b4abc8c8b8c99a7dd4e3a0449b4de064957
Unknown <jkm.svensson@gmail.com>
2019-01-24 15:06:00 +0100

    speedIncrease

--- Fly til you crash/Assets/SpeedController.cs
+++ Fly til you crash/Assets/SpeedController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpeedController : MonoBehaviour
 {
     float constSpeed = 20;
-    float AccelerationSpeed = 0.015f;
+    float AccelerationSpeed = 0.02f;
     float secondsToSpeedIncrease;
     float e = 2.7182818284590452353602874713527f;
     float speed;
@@ -21,11 +21,10 @@ public class SpeedController : MonoBehaviour
         if(Time.time > secondsToSpeedIncrease && Time.time < 300)
         {
             secondsToSpeedIncrease = Time.time + 1;
-            speed = Mathf.Pow((e), AccelerationSpeed * Time.time);
+            speed = Mathf.Pow((AccelerationSpeed * Time.time), e);
         }
         float float_velocity = constSpeed + speed;
         Camera.main.fieldOfView = Mathf.Clamp(float_velocity, 80f, 120f);
         rb.velocity = float_velocity * transform.forward;
-
     }
 }
