f0a464be0d640aa77118a9391eb7529cbf86fe89
Menoka <sebastian_kba@hotmail.com>
2019-01-24 14:54:54 +0100

    "Merged" tunnel/pipe generation
    Use Oscar Oders TunnelGenerator.

---
+++ Fly til you crash/Assets/Scripts/Generation/SpawnOnTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Script creator: Sebastian Nilsson, Oscar Oders 2019-01-24
+/// </summary>
+public class SpawnOnTrigger : MonoBehaviour
+{
+    internal TunnelGenarator generator;
+    private void OnTriggerEnter(Collider other)
+    {
+        generator.GenerateNewTunnelPiece();
+        Destroy(gameObject);
+    }
+}
