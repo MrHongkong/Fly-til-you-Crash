152a68460846789fc32a6e9504747bc6d516781b
Unknown <jkm.svensson@gmail.com>
2019-02-11 16:59:52 +0100

    Menu soon done

---
+++ Fly til you crash/Assets/Scripts/Menu/MenuFields.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFields : MonoBehaviour
+{
+    public GameObject[] fields;
+    public GameObject[] buttons;
+    public GameObject buttonParent;
+
+    void Start()
+    {
+        buttonParent.SetActive(false);
+        foreach (GameObject field in fields)
+        {
+            field.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        foreach (GameObject field in fields)
+        {
+            foreach(GameObject button in buttons)
+            {
+                if (button.activeInHierarchy)
+                {
+                    if (button.name != field.name)
+                    {
+                        field.SetActive(false);
+                    }
+                    else if (button.name == field.name) field.SetActive(true);
+                }
+            }
+        }
+    }
+}
