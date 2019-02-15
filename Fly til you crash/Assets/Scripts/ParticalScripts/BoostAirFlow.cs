4fc5dd452819c1d862f898d8dcc53744a17f7168
Oscar <Oders>
2019-02-15 09:14:36 +0100

    Boost particle settings

--- Fly til you crash/Assets/Scripts/ParticalScripts/BoostAirFlow.cs
+++ Fly til you crash/Assets/Scripts/ParticalScripts/BoostAirFlow.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class BoostAirFlow : MonoBehaviour
 {
     public GameObject BoostPartical;
+    public GameObject BoostParticalSmall;
+    private ParticleSystem part;
+    float trump = 6;
 
     void Start()
     {
         BoostPartical.SetActive(false);
+        BoostParticalSmall.SetActive(true);
+
+        part = BoostParticalSmall.GetComponent<ParticleSystem>();
     }
 
     void Update()
     {
-        if (Input.GetButton("Fastmotion"))
-        {
-           BoostPartical.SetActive(true);
+        trump += (trump < 50) ? 0.001f : 0;
+        
+        if (Input.GetButton("Slowmotion")) {
+            part.emissionRate = 0;
+        } else {
+            part.emissionRate = trump;
         }
-        else
-        {
-           BoostPartical.SetActive(false);
+
+        if (Input.GetButton("Fastmotion")) {
+            BoostPartical.SetActive(true);
+        } else {
+            BoostPartical.SetActive(false);
         }
     }   
 }
