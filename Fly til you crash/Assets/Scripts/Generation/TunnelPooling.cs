52eb15f839d99bafb97161a31782402ec042579a
BattleCisco <adam.torngren@hotmail.com>
2019-02-18 07:09:48 +0100

    Added new script for difficulty controlling

--- Fly til you crash/Assets/Scripts/Generation/TunnelPooling.cs
+++ Fly til you crash/Assets/Scripts/Generation/TunnelPooling.cs
@@ -4,43 +4,19 @@ using UnityEngine;
 
 public class TunnelPooling : MonoBehaviour
 {
-    [SerializeField] private GameObject[] prefabObjectsEasy, prefabObjectsMedium, prefabObjectsHard;
+    [SerializeField] private GameObject[] prefabObjects;
     [SerializeField] private int numberOfDuplicates;
-    private GameObject[] pooledObjectsEasy, pooledObjectsMedium, pooledObjectsHard;
-    [SerializeField] private int secondsToMedium, secondsToHard;
+    private GameObject[] pooledObjects;
     private void Awake()
     {
-        pooledObjectsEasy = new GameObject[prefabObjectsEasy.Length * numberOfDuplicates];
-        pooledObjectsMedium = new GameObject[(prefabObjectsEasy.Length + prefabObjectsMedium.Length) * numberOfDuplicates];
-        pooledObjectsHard = new GameObject[(prefabObjectsEasy.Length + prefabObjectsMedium.Length + prefabObjectsHard.Length) * numberOfDuplicates];
+        pooledObjects = new GameObject[prefabObjects.Length * numberOfDuplicates];
         int index = 0;
-        for(int i = 0; i < prefabObjectsEasy.Length; i++)
+        for(int i = 0; i < prefabObjects.Length; i++)
         {
             for (int j = 0; j < numberOfDuplicates; j++)
             {
-                pooledObjectsEasy[index] = Instantiate(prefabObjectsEasy[i]);
-                pooledObjectsMedium[index] = pooledObjectsEasy[index];
-                pooledObjectsHard[index] = pooledObjectsEasy[index];
-                pooledObjectsEasy[index].SetActive(false);
-                index++;
-            }
-        }
-        for(int i = 0; i < prefabObjectsMedium.Length; i++)
-        {
-            for (int j = 0; j < numberOfDuplicates; j++)
-            {
-                pooledObjectsMedium[index] = Instantiate(prefabObjectsMedium[i]);
-                pooledObjectsHard[index] = pooledObjectsMedium[index];
-                pooledObjectsMedium[index].SetActive(false);
-                index++;
-            }
-        }
-        for (int i = 0; i < prefabObjectsHard.Length; i++)
-        {
-            for (int j = 0; j < numberOfDuplicates; j++)
-            {
-                pooledObjectsHard[index] = Instantiate(prefabObjectsHard[i]);
-                pooledObjectsHard[index].SetActive(false);
+                pooledObjects[index] = Instantiate(prefabObjects[i]);
+                pooledObjects[index].SetActive(false);
                 index++;
             }
         }
@@ -49,12 +25,7 @@ public class TunnelPooling : MonoBehaviour
     {
         List<GameObject> unusedObjects;
 
-        if (Time.timeSinceLevelLoad > secondsToMedium)
-            unusedObjects = new List<GameObject>(pooledObjectsMedium);
-        else if(Time.timeSinceLevelLoad > secondsToHard)
-            unusedObjects = new List<GameObject>(pooledObjectsHard);
-        else
-            unusedObjects = new List<GameObject>(pooledObjectsEasy);
+        unusedObjects = new List<GameObject>(pooledObjects);
 
         for (int i = 0; i < activeObjects.Length; i++)
         {
