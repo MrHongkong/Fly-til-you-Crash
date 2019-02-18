2728bce2abdb2984d565381c5437cc51b2008ed8
Adam <adam.torngren@hotmail.com>
2019-02-18 16:05:42 +0100

    Unbinded highscore from displayhighscore

--- Fly til you crash/Assets/Scripts/Checker/DisplayHighscores.cs
+++ Fly til you crash/Assets/Scripts/Checker/DisplayHighscores.cs
@@ -10,31 +10,8 @@ public class DisplayHighscores : MonoBehaviour {
     //Code from Sebastian Lague
 
 	public TextMeshProUGUI[] highscoreFields;
-	Highscores highscoresManager;
-
-	void Start() {
-		for (int i = 0; i < highscoreFields.Length; i ++) {
-			highscoreFields[i].text = i + 1 + ". Fetching...";
-		}
-
-        highscoresManager = GetComponent<Highscores>();
-        StartCoroutine("RefreshHighscores");
-	}
 	
-	public void OnHighscoresDownloaded(Highscore[] highscoreList) {
-		for (int i = 0; i < highscoreFields.Length; i ++) {
-			highscoreFields[i].text = i+1 + ". ";
-			if (i < highscoreList.Length) {
-                string[] test =  highscoreList[i].username.Split('+');
-                highscoreFields[i].text += test[0] + " - " + highscoreList[i].score;
-			}
-		}
-	}
-
-    IEnumerator RefreshHighscores() {
-		while (true) {
-			highscoresManager.DownloadHighscores();
-			yield return new WaitForSeconds(30);
-		}
-	}
+    void OnEnable(){
+        Highscores.DownloadHighscoresToTMPro(highscoreFields);
+    }
 }
