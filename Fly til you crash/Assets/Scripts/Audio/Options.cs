94e9f4ae9905240d17ac1836d806dbc6d28c8546
Philip <philipa@hotmail.se>
2019-02-25 22:26:26 +0100

    Adding a volume slider to the options

---
+++ Fly til you crash/Assets/Scripts/Audio/Options.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class Options : MonoBehaviour
+{
+    
+    public Slider slider;
+    public AudioMixer audioMixer;
+
+    void Start()
+    {
+        if (slider == null) slider = GetComponentInChildren<Slider>();
+        if (slider != null) slider.value = PlayerPrefs.GetFloat("Volume");
+        GetMasterLevel();
+    }
+       
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
+
+    public void SetSlider()
+    {
+        slider = GameObject.Find("Canvas").GetComponentInChildren<Slider>();
+        if (slider != null) slider.value = PlayerPrefs.GetFloat("Volume");
+    }
+
+    public float GetMasterLevel()
+    {
+        float value;
+        bool result = audioMixer.GetFloat("Volume", out value);
+        if (result)
+        {
+            return value;
+            
+        }
+        else
+        {
+            return 0f;
+        }
+        
+    }
+
+
+}
